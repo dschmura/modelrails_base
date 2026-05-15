3566deb8810990d99bb54ee63c8d9c5070e3598f
Dave Chmura <dschmura@humbledaisy.com>
2026-05-15 13:18:18 -0400

    test(notifications): tighten singular aria-label guard with negative assertion

--- spec/helpers/notification_bell_helper_spec.rb
+++ spec/helpers/notification_bell_helper_spec.rb
@@ -93,7 +93,8 @@ RSpec.describe NotificationBellHelper, type: :helper do
       PasswordChangedNotifier.with(record: user).deliver(user)
 
       label = helper.avatar_button_aria_label(user)
-      expect(label).to include("1 unread notification")
+      expect(label).to include("1 unread notification,")
+      expect(label).not_to include("1 unread notifications")
       expect(label).to include("a security alert")
     end
 
