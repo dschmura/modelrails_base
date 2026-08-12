1ba071906805923bb56530c11fe7c97d59941405
Dave Chmura <dschmura@humbledaisy.com>
2026-08-12 10:34:42 -0400

    test(avatars): characterize invalid-source and failure-purge paths; assert source_unavailable flashes

--- spec/code_smells/flash_messages_are_asserted_spec.rb
+++ spec/code_smells/flash_messages_are_asserted_spec.rb
@@ -50,7 +50,6 @@ RSpec.describe "Flash messages are asserted, not just redirects" do
     "sessions.create.success",
     "sessions.destroy.success",
     "settings.avatars.destroy.success",
-    "settings.avatars.source_unavailable",
     "settings.avatars.update.rate_limited",
     "settings.avatars.update.success",
     "settings.connected_accounts.destroy.success",
